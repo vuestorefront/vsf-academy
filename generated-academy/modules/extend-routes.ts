eae06be87a75333fe2746d7cac49fb441a6ac21e
Marcin Sulowski <msulowski@vuestorefront.io>
2021-10-29 16:36:23 +0200

    cleanup

--- generated-academy/modules/extend-routes.ts
+++ generated-academy/modules/extend-routes.ts
@@ -1,11 +1,6 @@
 export default function () {
   this.extendRoutes(routes => {
     const routesToRemove = ['Category___', 'Product___', 'Brand__']
-    // const pathsToReplace = {
-    //   '/p/:id/:slug': '/product/:id/:slug',
-    //   '/c/:slug_1/:slug_2?/:slug_3?/:slug_4?/:slug_5?': '/category/:slug_1/:slug_2?/:slug_3?/:slug_4?/:slug_5?',
-    // }
-
     const pathsToReplace = [
       {
         oldPath: '/p/:id/:slug',
