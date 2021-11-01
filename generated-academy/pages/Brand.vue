c7cd413a1804752f6e24b90061468264f789bc65
Marcin Sulowski <msulowski@vuestorefront.io>
2021-11-01 19:40:02 +0100

    sadd heading component

--- generated-academy/pages/Brand.vue
+++ generated-academy/pages/Brand.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-    <h1>Brand - {{ $route.params.brand_name }}</h1>
-    <p>Check out our amazing offering from {{ $route.params.brand_name }}</p>
+    <SfHeading
+      :level="1"
+      :title="`Brand - ${$route.params.brand_name}`"
+      :description="`Check out our amazing offering from ${$route.params.brand_name}`"
+      class="sf-heading--left"
+    ></SfHeading>
   </div>
 </template>
-// <script>
-// export default {
-//   middleware({ route, error }) {
-//     const paramsEmpty = !Object.keys(route.params).length
-//     if (paramsEmpty) {
-//       error({ statusCode: 404 })
-//     }
-//   },
-// }
-// </script>
+<script>
+import { SfHeading } from '@storefront-ui/vue'
+export default {
+  components: {
+    SfHeading,
+  },
+}
+</script>
