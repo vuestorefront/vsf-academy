3b4a7866d8bab1ff2365215f02d805cb476640b0
Razz21 <razzrazz@wp.pl>
2022-02-09 14:14:13 +0100

    feat: custom integration version resolutions

--- helpers/getDependenciesList.js
+++ helpers/getDependenciesList.js
@@ -4,12 +4,18 @@ const paymentConfigs = require('../config/payment')
 const commerceConfigs = require('../config/commerce')
 
 module.exports = ({ commerce, cms, payment, search }) => {
-  const dependencies = {}
+  const dependencies = {},
+    resolutions = {};
 
   commerceConfigs[commerce] && Object.assign(dependencies, commerceConfigs[commerce].dependencies)
   cmsConfigs[cms] && Object.assign(dependencies, cmsConfigs[cms].dependencies)
   paymentConfigs[payment] && Object.assign(dependencies, paymentConfigs[payment].dependencies)
   searchConfigs[search] && Object.assign(dependencies, searchConfigs[search].dependencies)
 
-  return dependencies
+  commerceConfigs[commerce] && Object.assign(resolutions, commerceConfigs[commerce].resolutions)
+  cmsConfigs[cms] && Object.assign(resolutions, cmsConfigs[cms].resolutions)
+  paymentConfigs[payment] && Object.assign(resolutions, paymentConfigs[payment].resolutions)
+  searchConfigs[search] && Object.assign(resolutions, searchConfigs[search].resolutions)
+
+  return [ dependencies, resolutions ]
 }
